2db81ce98b0241d8ec07880e0602c91027a853b2
YeJeeO <yejeeo@yandex.ru>
2023-04-07 18:06:53 +0700

    modified:   components/Calendar.js

--- components/Calendar.js
+++ components/Calendar.js
@@ -11,7 +11,7 @@ const month=date.getMonth();
 return(
     <div className="calendar">
         <h1>Calendar</h1>
-        <input type="month" value={value} onChange={evt=>setValue(evt.target.value)}/>
+        <input type="date" value={value} onChange={evt=>setValue(evt.target.value)}/>
         <Month year={year} month={month}/>
     </div>
 )
